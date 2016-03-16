22de45f9d82e0e87374024c24a109513a1e9f764
399 <zkfop@qq.com>
2016-03-16 21:06:37 +0800

    new world

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Cache-Control" content="no-transform" />
+	<meta http-equiv="Cache-Control" content="no-siteapp" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="renderer" content="webkit|ie-comp|ie-stand">
+	<title>Hanns Seidel Stiftung</title>
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css" type="text/css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css" type="text/css">
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery1.42.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.SuperSlide.2.1.1.js"></script>
+</head>
+<body>
+<!--头部导航-->
+	<div class="header">
+		<div class="container">
+			<div class="logo"><a href="#">Hanns Seidel Stiftung</a></div>
+			<div class="g-row">
+				<ul class="nav">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Learn & Engage</a></li>
+					<li><a href="#">Resources</a></li>
+					<li><a href="#">Events</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+<!--头部导航END-->
+<!--幻灯片-->
+<div id="navWrap">
+	<div class ="navWrap title">
+		<h3>Text1</h3>
+	</div>
+	<div class="slideTxtBox">
+	    <div class="hd">
+	        <ul>
+		        <li>
+		       	 	<i class="flaticon-leaf"></i>Climate
+		        </li>
+		        <li>
+					 <i class="flaticon-cloudy"></i>Weather
+		        </li>
+		        <li>
+		        	<i class="flaticon-battery-charging-status"></i>Energy
+		        </li>
+		        <li>
+		        	<i class="flaticon-user"></i>Population
+		        </li>
+	        </ul>
+	    </div>
+	    <div class="bd">
+	        <ul>
+	            	<div class="bd-left">
+	            		<p><i class="flaticon-squares"></i>
+	            		Most of Namibia receives summer rainfall</p>
+	            	</div>
+	            	<div class="bd-right">
+						<p>Namibia is the driest country south of the Sahara, situated at the interface between different climate systems. Highly variable climatic conditions are the norm, and the country experiences frequent dry-spells and droughts with sporadic occurrences of flooding in river basins.</p>
+	            	</div>
+	            	<div class="clear"></div>
+	        </ul>
+	         <ul>
+	            	<div class="bd-left">
+	            		<p><i class="flaticon-squares"></i>
+	            		Namibia is characterised by high temperatures</p>
+	            	</div>
+	            	<div class="bd-right">
+						<p>Most of the country receives an annual average of more than nine hours of sunlight per day. Daytime air temperatures are generally warm but due to low humidity and outgoing long wave radiation at night, minimum temperatures can drop to below freezing point in winter. The north and south of the country experience the highest temperatures with the average maximum for the hottest month being over 34°C. The southern Kalahari experiences the lowest temperatures with the average minimum for the coldest month being less than 2°C.</p>
+	            	</div>
+	            	<div class="clear"></div>
+	        </ul>
+	         <ul>
+	            	<div class="bd-left">
+	            		<p><i class="flaticon-squares"></i>
+	            		Most of Namibia receives summer rainfall</p>
+	            	</div>
+	            	<div class="bd-right">
+						<p>Namibia is the driest country south of the Sahara, situated at the interface between different climate systems. Highly variable climatic conditions are the norm, and the country experiences frequent dry-spells and droughts with sporadic occurrences of flooding in river basins.</p>
+	            	</div>
+	            	<div class="clear"></div>
+	        </ul>
+	         <ul>
+	            	<div class="bd-left">
+	            		<p><i class="flaticon-squares"></i>
+	            		A population of 2.1 million people</p>
+	            	</div>
+	            	<div class="bd-right">
+						<p>Namibia has a population of 2.1 million people and has one of the lowest population densities in the world with an average of about 2.5 people per km2. About 62% of Namibians are living in rural areas and depend heavily on rain-fed agriculture, which is highly vulnerable to changes in climate, seasonal shifts and precipitation patterns.</p>
+	            	</div>
+	            	<div class="clear"></div>
+	        </ul>
+	    </div>
+	</div>
+</div>
+	<script type="text/javascript">jQuery(".slideTxtBox").slide(); </script>
+<!--幻灯片END-->
+</body>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/line-icon/font/flaticon.css"> 
+</html>
