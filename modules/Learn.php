629d0c3ed25420587601af23fd1457d6b89141d3
399 <zkfop@qq.com>
2016-03-21 14:22:53 +0800

    learn pages

---
+++ modules/Learn.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Template Name: Learn
+*/
+?>
+<?php get_header(); ?>
+<div class="text-head container">
+	<h2>LEARN & ENGAGE</h2>
+	<div class="learn">
+		<div class="learn-topic">
+			<a href="#" target="_blank">
+				<div class="topich">
+					<header class="topicnot">
+						<h3>Videos And Podcasts</h3>
+					</header>
+					<div class="topicbg"></div>
+					<img src="<?php bloginfo('template_directory'); ?>/img/video.jpg">
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class="learn">
+		<div class="learn-topic">
+			<a href="#" target="_blank">
+				<div class="topich">
+					<header class="topicnot">
+						<h3>Fact Sheets</h3>
+					</header>
+					<div class="topicbg"></div>
+					<img src="<?php bloginfo('template_directory'); ?>/img/sheets.jpg">
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class="learn">
+		<div class="learn-topic">
+			<a href="#" target="_blank">
+				<div class="topich">
+					<header class="topicnot">
+						<h3>Books</h3>
+					</header>
+					<div class="topicbg"></div>
+					<img src="<?php bloginfo('template_directory'); ?>/img/book.jpg">
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class="learn">
+		<div class="learn-topic">
+			<a href="#" target="_blank">
+				<div class="topich">
+					<header class="topicnot">
+						<h3>Posters/Infographics</h3>
+					</header>
+					<div class="topicbg"></div>
+					<img src="<?php bloginfo('template_directory'); ?>/img/infographics.jpg">
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
